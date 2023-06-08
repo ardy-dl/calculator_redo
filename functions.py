d2bc7d8250cbe8a00e58708e2541ada93d13b106
unknown <rosedenisejdeleon@iskolarngbayan.pup.edu.ph>
2023-06-08 13:43:00 +0800

    pseudocode

---
+++ functions.py
@@ -0,0 +1,10 @@
+class Operation:
+    def __init__(self, num_1, num_2):
+        self.num_1 = num_1
+        self.num_2 = num_2
+
+    def addition(self):
+        sum = num_1 + num_2
+        print(sum)
+
+        
