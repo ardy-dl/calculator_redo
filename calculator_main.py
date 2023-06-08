7e5b2e1e9a80f6bbbf822b5d67b10ed4bff55898
unknown <rosedenisejdeleon@iskolarngbayan.pup.edu.ph>
2023-06-08 14:16:58 +0800

    encountered an error

--- calculator_main.py
+++ calculator_main.py
@@ -1,6 +1,10 @@
 from calculator_ui import Calculator
+from functions import Operation
 
 calc = Calculator()
+solve = Operation()
 
 calc.input_num1()
 calc.input_num2()
+sum = solve.addition(num_1, num_2)
+print(sum)
--- functions.py
+++ functions.py
@@ -1,4 +1,14 @@
 # create operations class
+#create operations
+class Operation:
+    def addition(self, num_1, num_2):
+        sum = num_1 + num_2
+        return sum
+   
+
+    
+
+        
 # initialization
 # add
 # subtract
