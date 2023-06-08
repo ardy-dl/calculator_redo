aeb01a44cba55ae633638924032768c5766fc883
unknown <rosedenisejdeleon@iskolarngbayan.pup.edu.ph>
2023-06-08 13:50:39 +0800

    inputs working

---
+++ calculator_ui.py
@@ -0,0 +1,5 @@
+#ask user for operation
+#ask user for input 1 and 2
+num_1 = float(input("Enter a number: "))
+num_2 = float(input("Enter a number: "))
+#ask to try again
--- functions.py
+++ functions.py
@@ -1,10 +1,10 @@
-class Operation:
-    def __init__(self, num_1, num_2):
-        self.num_1 = num_1
-        self.num_2 = num_2
+# create operations class
+# initialization
+# add
+# subtract
+# multiply
+# divide
 
-    def addition(self):
-        sum = num_1 + num_2
-        print(sum)
+    
 
         
